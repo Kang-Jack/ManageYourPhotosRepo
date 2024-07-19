82941d2df9255608eaba59eb0ef2f3968ff7b308
lkang <lkang@slb.com>
2024-07-19 22:09:12 +0800

    generate tests

--- mtest/UnitTest1.cs
+++ mtest/UnitTest1.cs
@@ -1,47 +1,82 @@
 ﻿using System.Collections.Specialized;
 using foto_list;
-using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System.IO;
 using NUnit.Framework;
-namespace mtest
+namespace mtest;
+
+public class testableFotoManager : FotoManager
 {
-    public class testableFotoManager : FotoManager
+    public bool ReadListInFileRes { set; get; }
+
+    public string InputPhotoFolderRes { set; get; }
+
+    public string WriteListFileRes { set; get; }
+
+    protected override bool ReadListInFile(string listFileName, StringCollection allPhotos)
     {
-        public bool ReadListInFileRes{ set; get; }
-
-        public string InputPhotoFolderRes { set; get; }
-
-        public string WriteListFileRes { set; get; }
-
-        protected override bool ReadListInFile(string listFileName, StringCollection allPhotos)
-        {
-            return ReadListInFileRes;
-        }
-        protected override string InputPhotoFolderPath()
-        {
-            return InputPhotoFolderRes;
-        }
-        protected override string WriteListFile(string fileName, StringCollection allFiles)
-        {
-            return WriteListFileRes;
-        }
-
+        return ReadListInFileRes;
+    }
+    protected override string InputPhotoFolderPath()
+    {
+        return InputPhotoFolderRes;
+    }
+    protected override string WriteListFile(string fileName, StringCollection allFiles)
+    {
+        return WriteListFileRes;
     }
 
-    public class MTestFotoManger
+}
+public class MTestFotoManger
+{
+    IFotoManger m_sut;
+
+    [SetUp]
+    public void Setup()
     {
-        IFotoManger m_sut;
-        [SetUp]
-        public void Setup()
-        {
-            m_sut = new testableFotoManager();
-        }
+        m_sut = new testableFotoManager();
+    }
 
+    [TearDown]
+    public void TearDown()
+    {
+        m_sut = null;
+    }
 
-        [Test]
-        public void should_get_err_message_when_foto_path_invalide()
-        {
-            var re = m_sut.CreateListFile("errorPath");
-            Assert.AreEqual(re, ConstDef.ConstErrFotoPath);
-        }
+    [Test]
+    public void should_get_err_message_when_foto_path_invalide()
+    {
+        var re = m_sut.CreateListFile("errorPath");
+        Assert.AreEqual(re, ConstDef.ConstErrFotoPath);
+    }
+
+    [Test]
+    public void TestCreateListFile()
+    {
+        string listFileName = "testlist.txt";
+        string result = m_sut.CreateListFile(listFileName);
+        Assert.IsNotNull(result);
+        File.Delete(listFileName); // Clean up after test
+    }
+
+    [Test]
+    public void TestGenerateDiffReports()
+    {
+        string listFileName = "testlist.txt";
+        string reportFileName = "report.txt";
+        m_sut.CreateListFile(listFileName);
+        string result = m_sut.GenerateDiffReports(listFileName);
+        Assert.IsNotNull(result);
+        File.Delete(reportFileName); // Clean up after test
+    }
+    [Test]
+    public void TestCleanPhoto()
+    {
+        string listFileName = "testlist.txt";
+        string reportFileName = "report.txt";
+        m_sut.CreateListFile(listFileName);
+        string result = m_sut.CleanPhoto(listFileName, reportFileName);
+        Assert.IsNotNull(result);
+        File.Delete(reportFileName); // Clean up after test
     }
 }
+
